dad0ebcf8b08d90749c34b666b3bed99de585414
Sean Huang <castle2668@gmail.com>
2026-07-22 12:14:42 +0800

    Refactor(Module): Extract shared response headers into headers.js
    1. Move duplicated CORS and content-type headers into one module.
    2. Import the shared headers in errorHandle.
    
    Note:
    1. Applied per TA review feedback on the prep task.

--- errorHandle.js
+++ errorHandle.js
@@ -1,11 +1,6 @@
+const headers = require("./headers");
+
 function errorHandle(res) {
-  const headers = {
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Content-Type": "application/json",
-  };
   res.writeHead(400, headers);
   res.write(JSON.stringify({ status: "fail", message: "Invalid input" }));
   res.end();
